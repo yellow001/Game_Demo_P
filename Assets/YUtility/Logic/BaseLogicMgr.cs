a300f474b10ea74f1a30571b968d5685318a3f4b
yellowzhong001 <1144916875@qq.com>
2019-08-26 12:02:09 +0800

    同步迁移

---
+++ Assets/YUtility/Logic/BaseLogicMgr.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YUtility.Logic {
+    /// <summary>
+    /// 逻辑管理类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BaseLogicMgr<T> where T : BaseLogicMgr<T>
+    {
+        protected static T ins;
+        protected bool inited = false;
+        public static T Ins {
+            get {
+                if (ins == null)
+                {
+                    ins = new BaseLogicMgr<T>() as T;
+                }
+                return ins;
+            }
+        }
+
+        public virtual void Init()
+        {
+        }
+    }
+}
+
---
+++ Assets/YUtility/Logic/BaseLogicModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YUtility.Logic {
+    /// <summary>
+    /// 逻辑model类
+    /// </summary>
+    public class BaseLogicModel
+    {
+        /// <summary>
+        /// 事件
+        /// </summary>
+        public Dictionary<string, Action> m_EventDic;
+
+        public void AddEvent(string eventName,Action callBack) {
+            if (m_EventDic == null) {
+                m_EventDic = new Dictionary<string, Action>();
+            }
+            if (m_EventDic.ContainsKey(eventName))
+            {
+                m_EventDic[eventName] += callBack;
+            }
+            else {
+                m_EventDic[eventName] = callBack;
+            }
+        }
+
+        public void AddEvent(Enum eName, Action callBack) {
+            AddEvent(eName.ToString(), callBack);
+        }
+
+        public void RefreshEvent(string eventName) {
+            if (m_EventDic == null)
+            {
+                return;
+            }
+            if (m_EventDic.ContainsKey(eventName))
+            {
+                m_EventDic[eventName]();
+            }
+        }
+
+        public void RefreshEvent(Enum eName) {
+            RefreshEvent(eName.ToString());
+        }
+
+        public void RemoveEvent(string eventName,Action callBack) {
+            if (m_EventDic == null)
+            {
+                return;
+            }
+            if (m_EventDic.ContainsKey(eventName))
+            {
+                m_EventDic[eventName] -= callBack;
+            }
+        }
+
+        public void RemoveEvent(Enum eName,Action callBack) {
+            RemoveEvent(eName.ToString(),callBack);
+        }
+
+        public void RemoveAllCallBack(string eventName) {
+            if (m_EventDic == null)
+            {
+                return;
+            }
+            if (m_EventDic.ContainsKey(eventName))
+            {
+                m_EventDic.Remove(eventName);
+            }
+        }
+
+        public void RemoveAllCallBack(Enum eName) {
+            RemoveAllCallBack(eName.ToString());
+        }
+
+        public void ClearAllEvent() {
+            if (m_EventDic == null)
+            {
+                return;
+            }
+            m_EventDic.Clear();
+        }
+    }
+}
+
